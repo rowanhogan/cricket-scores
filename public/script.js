39ecbe54e0ed1f4dd7b1ed197ce6444a03523661
Rowan Hogan <rowan.hogan@gmail.com>
2015-05-12 23:28:26 +1000

    Scraper fixes, some style updates

--- public/script.js
+++ public/script.js
@@ -2,44 +2,56 @@
 var clientApp = angular.module('clientApp', ['ui.router'])
   .config(function($stateProvider) {
 
-    $stateProvider.state('matches', {
-      title: 'Matches',
-      url:   '',
-      views: {
-        'mainContent@': {
-          templateUrl: 'views/matches.html',
-          controller: 'MatchesCtrl',
-          resolve: {
-            preloaded: function(Match){
-              return Match.all();
-            }
+  $stateProvider.state('matches', {
+    title: 'Matches',
+    url:   '',
+    views: {
+      'mainContent@': {
+        templateUrl: 'views/matches.html',
+        controller: 'MatchesCtrl',
+        resolve: {
+          preloaded: function(Match){
+            return Match.all();
           }
         }
       }
-    });
-
-    $stateProvider.state('match', {
-      url: '/:series_id/:match_id',
-      views: {
-        'mainContent@': {
-          templateUrl: 'views/match.html',
-          controller: 'MatchCtrl',
-          resolve: {
-            preloaded: function(Match, $stateParams){
-              return Match.find($stateParams.series_id, $stateParams.match_id);
-            }
-          }
-        }
-      }
-    });
+    }
   });
 
-clientApp.controller('ApplicationCtrl', function($scope) {
-  // Global stuff
+  $stateProvider.state('match', {
+    url: '/:series_id/:match_id',
+    views: {
+      'mainContent@': {
+        templateUrl: 'views/match.html',
+        controller: 'MatchCtrl',
+        resolve: {
+          preloaded: function(Match, $stateParams){
+            return Match.find($stateParams.series_id, $stateParams.match_id);
+          }
+        }
+      }
+    }
+  });
 });
+
+clientApp.controller('ApplicationCtrl', function($rootScope) {
+
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    $rootScope.loading = true;
+  });
+
+  $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    $rootScope.loading = false;
+  });
+
+});
+
 
 clientApp.controller('MatchesCtrl', function($scope, $rootScope, preloaded, $interval) {
   $scope.matches = preloaded.matches;
+  $rootScope.loading = false;
+
+  Tinycon.setBubble('');
 
   if ($rootScope.live) {
     $interval.cancel($rootScope.live);
@@ -48,13 +60,34 @@ clientApp.controller('MatchesCtrl', function($scope, $rootScope, preloaded, $int
 
 clientApp.controller('MatchCtrl', function($scope, $location, $rootScope, preloaded, $stateParams, $interval) {
   $scope.match = preloaded;
-  $scope.scoreCard = $scope.match.d.MainScorecard.Sportsflash.Scorecard[0];
+  $scope.stateParams = $stateParams;
 
-  $scope.currentOver = $scope.scoreCard.Overs[0].Over[$scope.scoreCard.Overs[0].Over.length - 1];
+  $rootScope.loading = false;
 
-  $scope.batsmen = $scope.scoreCard.Batsmen[0].Batsman;
+  $('title').html($scope.match.score);
+  Tinycon.setBubble('');
 
-  $('title').html("(" + $scope.scoreCard.Score[0] + ") " + $scope.match.score);
+  if ($scope.match.d) {
+    $scope.scoreCard = $scope.match.d.MainScorecard.Sportsflash.Scorecard[0];
+
+    $scope.currentOver = $scope.scoreCard.Overs[0].Over[$scope.scoreCard.Overs[0].Over.length - 1];
+
+    $scope.batsmen = $scope.scoreCard.Batsmen[0].Batsman;
+
+    Tinycon.setBubble( parseInt($scope.scoreCard.Score[0].split('/')[0]) );
+
+    if ($scope.match.live) {
+      $rootScope.live = $interval(function() {
+        $.getJSON('/cricket/' + $stateParams.series_id + '/' + $stateParams.match_id, function(data) {
+          $scope.match = data;
+          $scope.scoreCard = $scope.match.d.MainScorecard.Sportsflash.Scorecard[0];
+          $('title').html("(" + $scope.scoreCard.Score[0] + ") " + $scope.match.score);
+          Tinycon.setBubble( parseInt($scope.scoreCard.Score[0].split('/')[0]) );
+        })
+      }, 30000);
+    }
+  }
+
   $scope.expanded = $location.search().expanded;
 
   $scope.toggleExpanded = function () {
@@ -68,18 +101,8 @@ clientApp.controller('MatchCtrl', function($scope, $location, $rootScope, preloa
 
     $scope.expanded = $location.search().expanded;
   }
-
-  Tinycon.setBubble( parseInt($scope.scoreCard.Score[0].split('/')[0]) );
-
-  $rootScope.live = $interval(function() {
-    $.getJSON('/cricket/' + $stateParams.series_id + '/' + $stateParams.match_id, function(data) {
-      $scope.match = data;
-      $scope.scoreCard = $scope.match.d.MainScorecard.Sportsflash.Scorecard[0];
-      $('title').html("(" + $scope.scoreCard.Score[0] + ") " + $scope.match.score);
-      Tinycon.setBubble( parseInt($scope.scoreCard.Score[0].split('/')[0]) );
-    })
-  }, 30000);
 });
+
 
 clientApp.service('Match', ['$http', '$q', function($http, $q) {
   var all = function all(options) {
--- server.rb
+++ server.rb
@@ -10,6 +10,17 @@ end
 
 get '/cricket/:series_id/:match_id' do
   data = JSON.parse(open("http://www.abc.net.au/dat/news/sport-data/live-scores/cricket/#{params[:series_id]}_#{params[:match_id]}_scorecard.json").read)
+
+  if data['d']['MainScorecard']['Sportsflash']['Header'][0]['$']['hasFullScorecard'] === "false"
+    data = {}
+    page = Nokogiri::HTML(open("http://abchtml.cricket.scoreboard.sportsflash.com.au/Default.aspx?pg=sm&ln=en-US&seriesId=#{params[:series_id]}&matchId=#{params[:match_id]}"))
+
+    data['series'] = page.css('#HeaderControl1_EventNameText')[0].children.to_s
+    data['match'] = page.css('#HeaderControl1_EventDetailsText')[0].children.to_s
+    data['score'] = page.css('.MidContainerResultText td')[0].children.to_s
+  end
+  # binding.pry
+
   data.to_json
 end
 
@@ -32,7 +43,12 @@ get '/matches' do
       rowData["team_#{i}"] = team.children[0].to_s
     end
 
-    rowData["start_time"] = row.css('script')[0].children[0].to_s.strip.gsub('localizeDate(','').gsub(');','').to_s
+    row.css('.HomeControlImgStyle img').each_with_index do |team, i|
+      rowData["team_#{i}_flag"] = team.attributes['src'].value
+    end
+
+    start_time = row.css('script')[0].children[0].to_s.strip.gsub('localizeDate("','').gsub('");','').to_s
+    rowData["start_time"] = start_time
 
     if rowData['venueId'].nil?
       matches.push rowData
